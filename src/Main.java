607d56d55449b47e6ced114b06043e95efd65280
ᑭOOᖇEᑌᗰ.KIᗰ♡ <111329305+kimpooreum@users.noreply.github.com>
2025-05-30 14:58:53 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        double salary = 55000.75;
+
+        int bonus = 5000;
+
+        double totalSalary = salary + bonus;
+
+        System.out.println("My Salary: " + totalSalary);
+
+    }
+}
